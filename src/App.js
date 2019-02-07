cab393deab7b02468200ed0f387344786e4cb36d
Niko Kivelä <niko@tovrleaf.com>
2019-02-08 00:03:12 +0200

    Add title param to Header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header title="Weather" />
       </div>
     );
   }
--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Header extends Component {
   render() {
     return (
-      <h1>Otsikko</h1>
+      <h1>{this.props.title}</h1>
     );
   }
 }
