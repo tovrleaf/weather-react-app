ad952f53b8f101854270a400acc5062036331cb1
Niko Kivelä <niko@tovrleaf.com>
2019-02-09 20:02:09 +0200

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import Header from './Header';
 import Form from './Form'
@@ -6,9 +7,19 @@ import Form from './Form'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header title="Weather" />
-        <Form />
+      <div className="wrapper">
+        <div className="main">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-5 title-container">
+              <Header title="Weather" />
+            </div>
+            <div className="col-xs-7 form-container">
+              <Form />
+            </div>
+          </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 class Header extends Component {
   render() {
     return (
-      <h1>{this.props.title}</h1>
+      <div>
+        <h1 className="title-container__title">{this.props.title}</h1>
+        <p className="title-container__subtitle">Enter city name...</p>
+      </div>
     );
   }
 }
