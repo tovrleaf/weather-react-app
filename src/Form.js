27a683fc19526ae995c17f328a01fe377b0d6c5d
Niko Kivelä <niko@tovrleaf.com>
2019-02-09 16:05:55 +0200

    Prepare to fetch weather

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 
 class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.handleCityChange = this.handleCityChange.bind(this);
+    this.fetchWeather = this.fetchWeather.bind(this);
+    this.state = {city: ''}
+  }
+  
+  handleCityChange(event) {
+    console.log('Setting town to ' + event.target.value)
+    this.setState({city: event.target.value})
+  }
+
+  fetchWeather(event) {
+    event.preventDefault();
+    console.log('Fetching weather for town ' + this.state.city)
+  }
+  
   render() {
     return (
-      <form>
-        <input type="text" name="city" placeholder="City" />
+      <form onSubmit={this.fetchWeather}>
+        <input type="text" name="city" value={this.props.city} onChange={this.handleCityChange} placeholder="City" />
         <button>Get Weather</button>
       </form>
     );
